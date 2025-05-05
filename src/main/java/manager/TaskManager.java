93f767259bd5289f4af0c38cb5f71d9ee9b39468
User Vadim <vadimkozuhov1812@yandex.ru>
2025-05-05 17:30:20 +0300

    Реализация истории просмотров с двусвязным списком и HashMap

---
+++ src/main/java/manager/TaskManager.java
@@ -0,0 +1,30 @@
+package manager;
+
+import model.*;
+import java.util.List;
+
+public interface TaskManager {
+    List<Task> getAllTasks();
+    void deleteAllTasks();
+    Task getTask(int id);
+    Task createTask(Task task);
+    void updateTask(Task task);
+    void deleteTask(int id);
+
+    List<Epic> getAllEpics();
+    void deleteAllEpics();
+    Epic getEpic(int id);
+    Epic createEpic(Epic epic);
+    void updateEpic(Epic epic);
+    void deleteEpic(int id);
+
+    List<Subtask> getAllSubtasks();
+    void deleteAllSubtasks();
+    Subtask getSubtask(int id);
+    Subtask createSubtask(Subtask subtask);
+    void updateSubtask(Subtask subtask);
+    void deleteSubtask(int id);
+
+    List<Subtask> getEpicSubtasks(int epicId);
+    List<Task> getHistory();
+}
---
+++ src/test/java/tests/EpicTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import model.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EpicTest {
+    @Test
+    void epicsWithSameIdShouldBeEqual() {
+        Epic epic1 = new Epic(1, "Epic 1", "Description 1");
+        Epic epic2 = new Epic(1, "Epic 2", "Description 2");
+
+        assertEquals(epic1, epic2);
+        assertEquals(epic1.hashCode(), epic2.hashCode());
+    }
+}
